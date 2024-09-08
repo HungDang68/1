9ac0dcf1b98eb308fe9492b7fed06d0f792e4d04
HungDang68 <hung200368@gmail.com>
2024-09-08 22:57:47 +0700

    3

--- HomePage/AndroidProject/app/src/main/java/com/example/githubbrowser/Create_Shortcuts_Activity.java
+++ HomePage/AndroidProject/app/src/main/java/com/example/githubbrowser/Create_Shortcuts_Activity.java
@@ -1,6 +1,7 @@
 package com.example.githubbrowser;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,7 @@ public class Create_Shortcuts_Activity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ImageButton backButton = findViewById(R.id.backButton_Create_Shortcuts_activity);
+        backButton.setOnClickListener(view -> getOnBackPressedDispatcher().onBackPressed());
     }
 }
--- HomePage/AndroidProject/app/src/main/java/com/example/githubbrowser/Shortcut_Activity.java
+++ HomePage/AndroidProject/app/src/main/java/com/example/githubbrowser/Shortcut_Activity.java
@@ -1,5 +1,6 @@
 package com.example.githubbrowser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -16,13 +17,22 @@ public class Shortcut_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_shortcut);
+        ImageButton saveButton = findViewById(R.id.imageButton_shortcut_create);
+        saveButton.setOnClickListener(v -> {
+            Intent intent01 = new Intent(Shortcut_Activity.this, Create_Shortcuts_Activity.class);
+            startActivity(intent01);
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button saveButton = findViewById(R.id.Shortcuts_save_button);
+
+
 
 
         ImageButton backButton = findViewById(R.id.backButton_Shortcuts_activity);
